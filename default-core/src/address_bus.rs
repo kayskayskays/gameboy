b2412afc92f1457131671b621933e51206f2fbbf
kayskayskays <kays6061@gmail.com>
2026-06-09 00:17:44 +1000

    Added a memory region for the interrupt enable register, tidied ranges

--- default-core/src/address_bus.rs
+++ default-core/src/address_bus.rs
@@ -6,6 +6,7 @@ pub(super) struct AddressBus {
     ram: [u8; 0x2000],
     oam: [u8; 0xA0],
     hram: [u8; 0x0100],
+    interrupt_enable: u8,
 }
 
 impl AddressBus {
@@ -16,6 +17,7 @@ impl AddressBus {
             ram: [0; 0x2000],
             oam: [0; 0xA0],
             hram: [0; 0x0100],
+            interrupt_enable: 0,
         }
     }
 
@@ -37,12 +39,13 @@ impl Index<u16> for AddressBus {
 
     fn index(&self, index: u16) -> &Self::Output {
         match index {
-            0x0000..=0x7FFF => &self.rom[index as usize],
-            0x8000..=0x9FFF => &self.vram[(index - 0x8000) as usize],
-            0xC000..=0xDFFF => &self.ram[(index - 0xC000) as usize],
-            0xFE00..=0xFE9F => &self.oam[(index - 0xFE00) as usize],
-            0xFEA0..=0xFEFF => unreachable!(),
-            0xFF80..=0xFFFE => &self.hram[(index - 0xFF80) as usize],
+            0x0000..0x8000 => &self.rom[index as usize],
+            0x8000..0xA000 => &self.vram[(index - 0x8000) as usize],
+            0xC000..0xE000 => &self.ram[(index - 0xC000) as usize],
+            0xFE00..0xFEA0 => &self.oam[(index - 0xFE00) as usize],
+            0xFEA0..0xFF00 => unreachable!(),
+            0xFF80..0xFFFF => &self.hram[(index - 0xFF80) as usize],
+            0xFFFF => &self.interrupt_enable,
             _ => todo!("unmapped: {:#06x}", index),
         }
     }
@@ -51,12 +54,13 @@ impl Index<u16> for AddressBus {
 impl IndexMut<u16> for AddressBus {
     fn index_mut(&mut self, index: u16) -> &mut Self::Output {
         match index {
-            0x0000..=0x7FFF => &mut self.rom[index as usize],
-            0x8000..=0x9FFF => &mut self.vram[(index - 0x8000) as usize],
-            0xC000..=0xDFFF => &mut self.ram[(index - 0xC000) as usize],
-            0xFE00..=0xFE9F => &mut self.oam[(index - 0xFE00) as usize],
-            0xFEA0..=0xFEFF => unreachable!(),
-            0xFF80..=0xFFFF => &mut self.hram[(index - 0xFF80) as usize],
+            0x0000..0x8000 => &mut self.rom[index as usize],
+            0x8000..0xA000 => &mut self.vram[(index - 0x8000) as usize],
+            0xC000..0xE000 => &mut self.ram[(index - 0xC000) as usize],
+            0xFE00..0xFEA0 => &mut self.oam[(index - 0xFE00) as usize],
+            0xFEA0..0xFF00 => unreachable!(),
+            0xFF80..0xFFFF => &mut self.hram[(index - 0xFF80) as usize],
+            0xFFFF => &mut self.interrupt_enable,
             _ => todo!("unmapped: {:#06x}", index),
         }
     }
